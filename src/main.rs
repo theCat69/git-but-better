576610b20aadc1f0ddccc7a657abbabe73e9ddd3
Felix Vadcard <vadcard.felix@gmail.com>
2023-05-25 08:00:29 +0200

    adding first feature -u switch on push parameter will automatically set upstream to origin on current branch name

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,58 @@
 use std::{
-    env,
-    io::{self, Write},
-    process::Command,
+    env::{self, Args},
+    iter::Skip,
+    process::{Command, Stdio},
 };
 
 fn main() {
-    let args: Vec<String> = env::args().skip(1).collect();
+    let mut cmd_iter = env::args().skip(1);
+
+    let git_main_param = cmd_iter.next().expect("No git main command");
+    let args: Vec<String>;
+
+    if git_main_param == "push" {
+        args = handle_push(cmd_iter);
+    } else {
+        args = cmd_iter.collect();
+    }
 
     let mut git_cmd = Command::new("git");
+    git_cmd.arg(git_main_param);
 
-    let output = git_cmd.args(args).output().expect("git command failed");
+    git_cmd
+        .args(args)
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .output()
+        .expect("git command failed");
+}
 
-    io::stdout().write_all(&output.stdout).unwrap();
-    io::stderr().write_all(&output.stderr).unwrap();
+fn handle_push(cmd_iter: Skip<Args>) -> Vec<String> {
+    let mut args = vec![];
+    for ele in cmd_iter {
+        if ele == "-u" {
+            args.push("-u".to_string());
+            args.push("origin".to_string());
+            args.push(get_current_git_branch());
+        } else {
+            args.push(ele);
+        }
+    }
+
+    return args;
+}
+
+fn get_current_git_branch() -> String {
+    return String::from_utf8(
+        Command::new("git")
+            .arg("branch")
+            .arg("--show-current")
+            .arg("2>nul")
+            .output()
+            .expect("Cannot get current branch")
+            .stdout,
+    )
+    .expect("stdout from command should be utf8 stream of bytes")
+    .trim()
+    .to_string();
 }
